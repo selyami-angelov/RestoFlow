6d4cf003ba4a6694dad33806f4e3a45ac5ff710d
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2023-06-27 16:52:20 +0300

    feat: restoflow-web init

--- restoflow-web/src/App.tsx
+++ restoflow-web/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Card } from 'flowbite-react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="https://www.flowbite-react.com/images/blog/image-1.jpg"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Card>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ restoflow-web/tailwind.config.js
@@ -0,0 +1,12 @@
+import * as flowbitePlugin from 'flowbite/plugin'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbitePlugin],
+}
