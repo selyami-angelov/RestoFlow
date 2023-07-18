14c4d1a902224419d7fce4583110f8dcd8df2470
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2023-07-18 23:07:06 +0300

    refactor: config

--- RestoFlow.Api/Program.cs
+++ RestoFlow.Api/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<RestoFlowDbContext>(options =>
     options.UseSqlServer(connectionString));
 
